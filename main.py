1519bb408f6c7a26a6b06df1f7698757f15f8700
Tolgonai Mamytova <tolgonai.mamytova.ch@gmail.com>
2025-04-25 12:17:12 +0600

    hello world!

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(Tolgonai):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello Tolgonai!Dastanova!Mamytova!, {Mamytova}!Tolgonai!')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hello World, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
